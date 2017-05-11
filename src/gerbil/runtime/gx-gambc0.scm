ea09559988bc91cf3ea3f59148081f4978129445
vyzo <vyzo@hackzen.org>
2017-05-11 18:52:12 +0300

    Gerbil v0.7

--- src/gerbil/runtime/gx-gambc0.scm
+++ src/gerbil/runtime/gx-gambc0.scm
@@ -16,7 +16,7 @@
 ;;;
 
 (define (gerbil-version-string)
-  "v0.7-DEV")
+  "v0.7")
 
 (define (gerbil-system-version-string)
   (string-append "Gerbil " (gerbil-version-string) " on Gambit " (system-version-string)))
