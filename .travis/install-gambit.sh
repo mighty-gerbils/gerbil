093210d6fdbf99406497ce503abf64b2b6d5c9ac
paul <paul@tymate.com>
2018-03-10 17:01:05 +0100

    ci: add gambit --enable-single-host flag

--- .travis/install-gambit.sh
+++ .travis/install-gambit.sh
@@ -4,7 +4,7 @@ if [ ! -f $HOME/gambit/bin/gsi ]; then
   wget http://www.iro.umontreal.ca/~gambit/download/gambit/v4.8/source/gambit-v4_8_9-devel.tgz
   tar xzvf gambit-v4_8_9-devel.tgz
   pushd gambit-v4_8_9-devel
-  ./configure --prefix=$HOME/gambit
+  ./configure --prefix=$HOME/gambit --enable-single-host
   make
   make install
   make bootstrap
