e40b4b60da7159417ec6d6bef66d2310398b1a78
paul <paul@tymate.com>
2018-03-10 14:50:06 +0100

    ci: add basic .travis.yml

---
+++ .travis/install-gambit.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [ ! -f $HOME/gambit/bin/gsi ]; then
+  wget http://www.iro.umontreal.ca/~gambit/download/gambit/v4.8/source/gambit-v4_8_9-devel.tgz
+  tar xzvf gambit-v4_8_9-devel.tgz
+  pushd gambit-v4_8_9-devel
+  ./configure --prefix=$HOME/gambit
+  make
+  make install
+  make bootstrap
+  popd
+fi
+
+sudo ln -sf $HOME/gambit/bin/gsi /bin/gsi
+sudo ln -sf $HOME/gambit/bin/gsi-script /bin/gsi-script
+sudo ln -sf $HOME/gambit/bin/gsc /bin/gsc
+
