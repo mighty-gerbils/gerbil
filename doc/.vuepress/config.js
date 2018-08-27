58b4f065efd33afa72af8a4d9ff3d9c21567bdb0
Sebastien Armand <sebastien@squareup.com>
2018-08-26 21:15:48 -0700

    fix help us repo link and directory name

--- doc/.vuepress/config.js
+++ doc/.vuepress/config.js
@@ -36,9 +36,9 @@ module.exports = {
         // Optional options for generating "Edit this page" link
 
         // if your docs are in a different repo from your main project:
-        docsRepo: 'khepin/gerbil-docs',
+        docsRepo: 'vyzo/gerbil',
         // if your docs are not at the root of the repo:
-        docsDir: 'docs',
+        docsDir: 'doc',
         // if your docs are in a specific branch (defaults to 'master'):
         docsBranch: 'master',
         // defaults to false, set to true to enable
