ca5524ec4c9461ec6fb1c01d37f47fd3ff6c135e
vyzo <vyzo@hackzen.org>
2018-09-06 14:34:14 +0300

    travis: use gambit-v4.9.0

--- .travis/install-gambit.sh
+++ .travis/install-gambit.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [ ! -f $HOME/gambit/bin/gsi ]; then
-  wget http://www.iro.umontreal.ca/~gambit/download/gambit/v4.8/source/gambit-v4_8_9-devel.tgz
-  tar xzvf gambit-v4_8_9-devel.tgz
-  pushd gambit-v4_8_9-devel
+  wget http://www.iro.umontreal.ca/~gambit/download/gambit/v4.9/source/gambit-v4_9_0.tgz
+  tar xzvf gambit-v4_9_0.tgz
+  pushd gambit-v4_9_0
   ./configure --prefix=$HOME/gambit --enable-single-host
   make
   make install
@@ -13,4 +13,3 @@ fi
 sudo ln -sf $HOME/gambit/bin/gsi /bin/gsi
 sudo ln -sf $HOME/gambit/bin/gsi-script /bin/gsi-script
 sudo ln -sf $HOME/gambit/bin/gsc /bin/gsc
-
