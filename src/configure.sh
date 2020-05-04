6fe643eb5224c090aee597b172857963233f664e
Jason Felice <jason.m.felice@gmail.com>
2020-05-04 17:56:49 -0400

    Allow --with-gsc

--- src/configure.sh
+++ src/configure.sh
@@ -2,6 +2,7 @@
 set -eu
 
 readonly FEATURES='libxml libyaml zlib sqlite mysql lmdb leveldb'
+readonly OPTIONS='gsc'
 
 enable_libxml='#f'
 enable_libyaml='#f'
@@ -10,6 +11,8 @@ enable_sqlite='#t'
 enable_mysql='#f'
 enable_lmdb='#f'
 enable_leveldb='#f'
+
+with_gsc='gsc'
 
 feature_exists() {
   case " $FEATURES " in
@@ -28,6 +31,23 @@ set_feature_enable() {
   eval "enable_$feature='$enable'"
 }
 
+option_exists() {
+  case " $OPTIONS " in
+    *" $1 "*) return 0;;
+  esac
+  return 1
+}
+
+set_option() {
+  option="$1"
+  value="$2"
+  if ! option_exists "$option"; then
+    printf 'configure.sh: unknown option "%s".\n' "$option" >&2
+    exit 1
+  fi
+  eval "with_$option='$value'"
+}
+
 write_build_features() {
   (
     for feature in $FEATURES; do
@@ -41,6 +61,10 @@ while [ $# -gt 0 ]; do
   case "$1" in
     --enable-*)  set_feature_enable "${1#--enable-}"  '#t';;
     --disable-*) set_feature_enable "${1#--disable-}" '#f';;
+    --with-*=*)
+      arg="${1#--with-}"
+      set_option "${arg%=*}" "${arg#*=}"
+      ;;
     *)
       printf 'configure.sh: unknown argument "%s".\n' "$1" >&2
       exit 1
