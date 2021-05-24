bd79c2eb2cf39f6d6050a074d41308ba8de59b82
Drew Crampsie <me@drewc.ca>
2021-05-24 13:55:20 -0700

    Add the start of "--with-gambit"

--- doc/generate-configure-and-make.sh
+++ doc/generate-configure-and-make.sh
@@ -9,5 +9,5 @@ tangle() {
 }
 
 # Once all that is done, run autoconf.
-cd $GERBIL_SOURCE && tangle ./doc/configure-and-make.org && autoconf -V && \
-    echo Generated ./configure and ./Makefile
+cd $GERBIL_SOURCE && tangle ./doc/configure-and-make.org && echo "HERE?" `pwd` && autoconf && \
+    echo Generated ./configure and ./Makefile,in ? `ls configure Makefile.in`
