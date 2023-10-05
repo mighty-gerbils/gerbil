db64e8d0b32d3b4e0cac0c16b8703faa8490b66c
vyzo <vyzo@hackzen.org>
2023-10-06 01:26:32 +0300

    reorder guide pages, macos before nix

--- doc/.vuepress/config.js
+++ doc/.vuepress/config.js
@@ -14,7 +14,7 @@ module.exports = {
           '/guide/': [
               {
                   title: 'Guide',
-                  children: ['', 'intro', 'getting-started', 'ffi', 'emacs', 'shell', 'env-vars', 'package-manager', 'docker', 'nix', 'r7rs', 'macos']
+                  children: ['', 'intro', 'getting-started', 'ffi', 'emacs', 'shell', 'env-vars', 'package-manager', 'docker', 'macos', 'nix', 'r7rs']
               }
           ],
           '/tutorials/': [
