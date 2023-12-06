e3284b820385fedb1cec8967cfd360ca6b6c8c0a
Drew Crampsie <me@drewc.ca>
2023-12-05 17:19:01 -0700

    Update Homebrew Formula for 0.18.1 (#1075)
    Points to the previous commit that _should_ be near 0.18.1.

--- homebrew/gerbil-scheme.rb
+++ homebrew/gerbil-scheme.rb
@@ -4,9 +4,9 @@ class GerbilScheme < Formula
   homepage "https://cons.io"
   license any_of: ["LGPL-2.1-or-later", "Apache-2.0"]
   url "https://github.com/mighty-gerbils/gerbil.git", using: :git,
-      revision: "8ca36a928bc9345f9d28e5f2dfcb55ca558e85f9"
+      revision: "0917172a519d28c7833886b445938fd250717b2a"
   head "https://github.com/mighty-gerbils/gerbil.git", using: :git
-  version "0.18"
+  version "0.18.1"
   revision 1
   depends_on "openssl@3"
   depends_on "sqlite"
@@ -16,25 +16,20 @@ class GerbilScheme < Formula
   fails_with :clang do
     cause "gerbil-scheme is built with GCC"
   end
-
-
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
     ENV.deparallelize
     system "make", "install"
-
     rm prefix/"bin"
     mkdir prefix/"bin"
 
     cd prefix/"current/bin" do
-      ln "gerbil", prefix/"bin", verbose: true
-      cp %w(gxc gxensemble gxi gxpkg gxprof gxtags gxtest), prefix/"bin"
+       ln "gerbil", prefix/"bin", verbose: true
+       cp %w(gxc gxensemble gxi gxpkg gxprof gxtags gxtest), prefix/"bin"
+     end
+  end
+    test do
+      assert_equal "0123456789", shell_output("#{bin}/gxi -e \"(for-each write '(0 1 2 3 4 5 6 7 8 9))\"")
     end
-  end
-
-  test do
-    assert_equal "0123456789", shell_output("#{bin}/gxi -e \"(for-each write '(0 1 2 3 4 5 6 7 8 9))\"")
-  end
-
 end
