e767fad79a28972936b32db93e596b7276e61011
vyzo <vyzo@hackzen.org>
2024-08-28 00:47:45 +0300

    link to the tuorial

--- doc/.vuepress/config.js
+++ doc/.vuepress/config.js
@@ -22,7 +22,7 @@ module.exports = {
           '/tutorials/': [
               {
                   title: 'Tutorials',
-                  children: ['', 'languages', 'kvstore', 'proxy', 'httpd', 'ensemble']
+                  children: ['', 'languages', 'kvstore', 'proxy', 'httpd', 'ensemble', 'advanced-ensemble']
               }
           ], '/reference/gerbil/runtime/': [
 	          { title: "Gerbil Runtime",
