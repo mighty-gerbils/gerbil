e7e3012e102d45e4a3f7725f4ad9c92e599a5954
Drew Crampsie <me@drewc.ca>
2026-03-31 12:58:01 -0700

    Gambit changes for no git build

---
+++ src/gambit/include/stamp.h
@@ -0,0 +1,13 @@
+/* Automatically generated */
+
+#ifndef ___STAMP_VERSION
+#define ___STAMP_VERSION "v4.9.7-6-g64f4d369"
+#endif
+
+#ifndef ___STAMP_YMD
+#define ___STAMP_YMD 20250817
+#endif
+
+#ifndef ___STAMP_HMS
+#define ___STAMP_HMS 174740
+#endif
